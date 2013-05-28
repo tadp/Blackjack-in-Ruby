9a37d253a92fce4ae37826bda1c46f1d1af92f4c
tadp <june@tutormentor.org>
2013-05-28 12:14:33 -0700

    websolution4_partial

--- BlackjackWebApp/main.rb
+++ BlackjackWebApp/main.rb
@@ -25,7 +25,33 @@ helpers do
     total
   end
   #calculate_total(session[:dealers_cards]) => 20
+
+
+
+def card_image(card) #['H','4']
+  suit = case card[0]
+    when 'H' then 'hearts'
+    when 'D' then 'diamonds'
+    when 'C' then 'clubs'
+    when 'S' then 'spades'
+  end
+
+
+  value = card[1]
+  if ['J', 'Q', 'K','A'].include(value)
+    value = case card[1]
+      when 'J' then 'jack'
+      when 'Q' then 'queen'
+      when 'K' then 'king'
+      when 'A' then 'ace'
+    end
+  end
+
+  "<img src='/images/cards/#{suit}_#{value}.jpg'>"
 end
+end
+
+
 
 before do
   @show_hit_or_stay_buttons = true
@@ -53,7 +79,7 @@ end
 get '/game' do
   #create a deck and put it in session
   suits = ['H', 'D','C','S']
-  values = '2345678910JQKA'.split('')
+  values = '2','3','4','5','6','7','8','9','10','J','Q','K','A'
   session[:deck]=suits.product(values).shuffle! #[ ['H','9'], ['C','K']...]
   #deal cards
   session[:dealer_cards] = []
